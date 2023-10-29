bc6e3b220cba4d742fb0e7db276aa5a61b628d0f
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2023-10-28 19:10:24 -0500

    Rest Api Go Fiber Gorm

---
+++ config/database.go
@@ -0,0 +1,29 @@
+// @/config/database.go
+package config
+
+import (
+	"github.com/JhonLudenaReyes/gorm/entities"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Database *gorm.DB
+var DATABASE_URI string = "root:admin@tcp(localhost:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+
+func Connect() error {
+	var err error
+
+	Database, err = gorm.Open(mysql.Open(DATABASE_URI), &gorm.Config{
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	Database.AutoMigrate(&entities.Dog{})
+
+	return nil
+}
